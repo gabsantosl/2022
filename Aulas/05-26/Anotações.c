672da6b690b07d68447c1b989fdf61a4b2272dff
gab <82168937+gabsantosl@users.noreply.github.com>
2022-05-26 10:48:22 -0300

    Add files via upload

---
+++ Aulas/05-26/Anotações.c
@@ -0,0 +1,19 @@
+/*
+=== ESTRUTRA DO...WHILE ===
+
+	int i = 0;
+	
+	do{
+		printf("%d\n", i); - comando
+		i++; - contador	
+	}while(i <= 20); - condição 
+	
+=== ESTRUTRA WHILE ===
+
+	int i = 0;
+	
+	while(i <= 20){ - condição
+		printf("%d\n", i); - comando
+		i++; - contador
+	}
+*/
