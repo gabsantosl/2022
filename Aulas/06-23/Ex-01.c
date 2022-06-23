2fac54194335a150b86d946f96165772d6ccb495
gab <82168937+gabsantosl@users.noreply.github.com>
2022-06-23 10:26:22 -0300

    Add files via upload

---
+++ Aulas/06-23/Ex-01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	PROPRIEDADES DAS VARIÁVEIS:
+	- tipo
+	- nome
+	- valor
+	
+	DIFERENÇCAS ENTRE FOR E WHILE
+	- for possui contador
+	- for só aceita variável inteira
+	- do... while... verifica a condição depois de executar o programa
+	
+	DIFERENÇAS ENTRE IF... ELSE E SWITCH... CASE
+	- switch só aceota variável inteira e char (singular)
+*/
+
+int main() {
+	
+	return 0;
+}
